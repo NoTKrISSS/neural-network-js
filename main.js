d0d3859c19c82e8922aefd2fd3778f1ab9505e90
NoTKrISSS <46658821+NoTKrISSS@users.noreply.github.com>
2022-11-21 16:35:10 +0200

    sensor and new utils

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 animate();
 
 function animate() {
-  car.update();
+  car.update(road.borders);
   canvas.height = window.innerHeight;
   ctx.save();
   //makes road move you can change the car start position here
