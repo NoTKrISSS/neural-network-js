159b05b3a3d434dd5bb57a940612dca0cfb38b2d
NoTKrISSS <46658821+NoTKrISSS@users.noreply.github.com>
2022-11-21 19:54:01 +0200

    added traffic and traffic collision & sensor functionality to traffic

--- sensor.js
+++ sensor.js
@@ -1,23 +1,23 @@
 class Sensor {
   constructor(car) {
     this.car = car;
-    this.rayCount = 6;
+    this.rayCount = 280;
     this.rayLength = 150;
-    this.raySpread = Math.PI / 2;
+    this.raySpread = Math.PI / 1;
 
     this.rays = [];
     this.readings = [];
   }
 
-  update(roadBorders) {
+  update(roadBorders, traffic) {
     this.#castRays();
     this.readings = [];
     for (let i = 0; i < this.rays.length; i++) {
-      this.readings.push(this.#getReading(this.rays[i], roadBorders));
+      this.readings.push(this.#getReading(this.rays[i], roadBorders, traffic));
     }
   }
 
-  #getReading(ray, roadBorders) {
+  #getReading(ray, roadBorders, traffic) {
     let touches = [];
 
     for (let i = 0; i < roadBorders.length; i++) {
@@ -29,6 +29,21 @@ class Sensor {
       );
       if (touch) {
         touches.push(touch);
+      }
+    }
+
+    for (let i = 0; i < traffic.length; i++) {
+      const poly = traffic[i].polygon;
+      for (let j = 0; j < poly.length; j++) {
+        const value = getIntersection(
+          ray[0],
+          ray[1],
+          poly[j],
+          poly[(j + 1) % poly.length]
+        );
+        if (value) {
+          touches.push(value);
+        }
       }
     }
 
